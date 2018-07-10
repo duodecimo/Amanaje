b4f8c5dda2b116c84b608438bd16ea03463507d3
duodecimo <duodecimo@gmail.com>
2018-07-10 16:15:42 -0300

    !

--- python/gui.py
+++ python/gui.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # by duo Dec 2017
 
+import tkinter
 import cv2
 import time
 import numpy as np
@@ -8,58 +9,48 @@ import os
 import argparse
 from datetime import datetime
 import shutil
+from PIL import Image, ImageTk
 
-def show_webcam(mirror=False, numCam=-1):
-  global img, txt
-  txt = ' '
-  frequency = 100 # Hertz
-  duration  = 50 # milliseconds
-  cam = cv2.VideoCapture(numCam)
-  time.sleep(0.5)
-  start_time = time.time()
-  cv2.namedWindow('Amanajé', cv2.WINDOW_NORMAL)
-  while True:
-    ret_val, img = cam.read()
-    if mirror: 
-      img = cv2.flip(img, 1)
-    height, width = img.shape[1::-1]
-    cv2.setMouseCallback('Amanajé', onMouse)
-    cv2.putText(img, txt, (int(width*0.5), int(height*0.05)), cv2.FONT_HERSHEY_TRIPLEX, 0.8, (255, 0, 0), 2, cv2.LINE_AA)
-    cv2.imshow('Amanajé', img)
-    elapsed_time = time.time() - start_time
-    #if cv2.waitKey(1) & 0xFF == ord('q'):
-    if cv2.waitKey(1) & 0xFF == 27:
-      break  # esc to quit
-  cv2.destroyAllWindows()
+global drawing
 
-def onMouse(event, x, y, flags, param):
-  # grab references to the global variables
-  global refPt, cropping, img, txt
-  cropping = False
-  # if the left mouse button was clicked, record the starting
-  # (x, y) coordinates and indicate that cropping is being
-  # performed
-  if event == cv2.EVENT_LBUTTONDOWN:
-    refPt = [(x, y)]
-    cropping = True
- 
-  # check to see if the left mouse button was released
-  elif event == cv2.EVENT_LBUTTONUP:
-    # record the ending (x, y) coordinates and indicate that
-    # the cropping operation is finished
-    refPt.append((x, y))
-    cropping = False
- 
-  # draw a rectangle around the region of interest
-  #cv2.rectangle(image, refPt[0], refPt[1], (0, 255, 0), 2)
+width, height = 800, 600
+cam = cv2.VideoCapture(-1)
+cam.set(cv2.CAP_PROP_FRAME_WIDTH, width)
+cam.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
+
+window = tkinter.Tk()
+window.title('Amanajé')
+window.bind('<Escape>', lambda e: window.quit())
+
+canvas = tkinter.Label(window)
+canvas.pack()
+drawing = False
+txt = ' '
+
+def show_webcam():
+  global drawing
+  _, frame = cam.read()
+  frame = cv2.flip(frame, 1)
+  x = window.winfo_pointerx() - window.winfo_rootx()
+  y = window.winfo_pointery() - window.winfo_rooty()
   txt = 'Mouse : (' + str(x) + ', ' + str(y) + ')'
-  if cropping:
-    txt += ' cropping'
-  else:
-    txt += ' not cropping'
+  if drawing:
+    txt += ' drawing'
+  cv2.putText(frame, txt, (int(width*0.3), int(height*0.05)), cv2.FONT_HERSHEY_TRIPLEX, 0.6, (255, 0, 0), 1, cv2.LINE_AA)
+  cv2image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGBA)
+  img = Image.fromarray(cv2image)
+  imgtk = ImageTk.PhotoImage(image=img)
+  canvas.imgtk = imgtk
+  canvas.configure(image=imgtk)
+  canvas.after(10, show_webcam)
 
-def main():
-  show_webcam(mirror=False, numCam=0)
+def toggleDraw():
+  global drawing
+  drawing = not drawing
 
+btn_snapshot=tkinter.Button(window, text="Draw", width=50, command=toggleDraw)
+btn_snapshot.pack(anchor=tkinter.CENTER, expand=False)
 
-main()
+show_webcam()
+window.mainloop()
+
